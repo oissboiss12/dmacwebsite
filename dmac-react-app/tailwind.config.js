ff4c5f1f828b6440416d7a1d0d2a42d4163fc17a
oissboiss12 <oisinluke80@gmail.com>
2024-06-17 12:33:16 +0100

    commit

---
+++ dmac-react-app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkmode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  varients: {
+    extend: {},
+  },
+  plugins: [],
+}
+
